0814f8a09b0435447b4b1e2c8fb263ae9187b051
farhan0212 <farhan.ramadan0212@gmail.com>
2024-07-28 19:07:29 +0700

    penambahan form dan validasi

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,13 +1,68 @@
+"use client";
+
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../ui/input";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
     <CardWrapper
       headerLabel="Welcome Back"
       backButtonLabel="Back to Home"
       backButtonHref="/auth/register"
       showSocial>
-      login
+      <Form {...form}>
+        <form onSubmit={() => {}} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    {...field}
+                    placeholder="JohnDoe@example.com"
+                    type="email"
+                  />
+                  <FormMessage />{" "}
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input {...field} type="password" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
