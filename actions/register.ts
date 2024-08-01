112c93ddd12fa5ea5fbe1ab919383c4c5d07997d
farhan0212 <farhan.ramadan0212@gmail.com>
2024-08-01 22:45:48 +0700

    penambahan clerk

--- actions/register.ts
+++ actions/register.ts
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import { RegisterSchema } from "@/schemas/index";
 import * as z from "zod";
 import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -13,7 +14,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const { email, password, name } = validateFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const existingUser = await db.user.findUnique({ where: { email } });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) {
     return { error: "email already exists" };
---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { auth } from "@/auth";
+import { Console } from "console";
+
+export default auth((req) => {
+  console.log("ROUTE: " + req.nextUrl.pathname);
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
