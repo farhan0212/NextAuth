d52da6937a05d1768462bf849e09133028f05047
farhan0212 <farhan.ramadan0212@gmail.com>
2024-08-15 22:49:30 +0700

    susah banget

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
-import type { NextAuthOptions, Session } from "next-auth";
-
 import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
-
+import { NextAuthRequest, NextApiResponse } from "next";
 import {
   DEFAULT_LOGIN_REDIRECT,
   apiAuthPrefix,
@@ -12,31 +10,33 @@ import {
 
 const { auth } = NextAuth(authConfig);
 
-export default auth(
-  (req: NextAuthOptions["req"], res: NextAuthOptions["res"]) => {
-    const { nextUrl } = req;
-    const isLoggedIn = !!req.session;
+export default auth((req: NextAuthRequest, res: NextApiResponse) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
 
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    if (isApiAuthRoute) {
-      return null;
-    }
-
-    if (isAuthRoute) {
-      if (isLoggedIn) {
-        return res.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-      }
-      return null;
-    }
-
-    if (!!isLoggedIn && !isPublicRoute) {
-      return res.redirect(new URL("/auth/login"));
-    }
+  if (isApiAuthRoute) {
+    return null;
   }
-);
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      res.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl).toString());
+      return;
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    res.redirect(new URL("/auth/login", nextUrl).toString());
+    return;
+  }
+
+  return null;
+});
 
 export const config = {
   matcher: [
